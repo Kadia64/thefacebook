f3af35ebed9fa6b4a0e85ad9a8c042744017d099
wesgl256 <wesgl@WORKSTATION-256>
2025-03-12 18:41:06 -0500

    included the Dotenv package for environment variables

---
+++ functions/scripts/database-handler.php
@@ -0,0 +1,43 @@
+<?php 
+$_PATH = '/projects/thefacebook/functions/';
+require_once $_SERVER['DOCUMENT_ROOT'].$_PATH.'scripts/content.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/projects/thefacebook/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+class DatabaseHandler {
+    
+    private static $pdo = null;
+    private function __construct() {}
+
+
+    public static function getConnection() {
+        if (self::$pdo == null) {
+
+            $dotenv = Dotenv::createImmutable('/opt/lampp/secure/');
+            $dotenv->load();
+
+            try {
+                self::$pdo = new PDO("mysql:host=".$_ENV["SERVER"].";dbname=".$_ENV["DATABASE"]."", $_ENV["USERNAME"], $_ENV["PASSWORD"]);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Database connection failed:\n ".$e->getMessage());
+            }
+            
+            return self::$pdo;
+        }
+    }
+}
+class SQLHandler {
+
+
+    public static function TEST_getTables() {
+        $pdo = DatabaseHandler::getConnection();
+
+        $test = $pdo->query("SHOW TABLES");
+        
+        $tables = $test->fetchAll(PDO::FETCH_COLUMN);
+        print_r($tables);
+    }
+}
+?>
